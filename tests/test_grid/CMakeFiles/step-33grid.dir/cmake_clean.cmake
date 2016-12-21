97eb01a11afdfd3b048e0e8d9301f68f2b34d5a8
Jane Lee <jane.lee@maths.ox.ac.uk>
2016-12-21 17:24:22 +0000

    X

---
+++ tests/test_grid/CMakeFiles/step-33grid.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/step-33grid.dir/step-33grid.cc.o"
+  "step-33grid.pdb"
+  "step-33grid"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/step-33grid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
