fe0e532e1a8cf1ae4e7b27106ec7d54f5e71930d
Jane Lee <jane.lee@maths.ox.ac.uk>
2017-01-17 14:44:45 +0000

    putting up the allinone tests for step-33 with mesh and conditions in the main source file

---
+++ tests/test_allinone/CMakeFiles/step-33.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/step-33.dir/step-33.cc.o"
+  "step-33.pdb"
+  "step-33"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/step-33.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
